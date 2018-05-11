dfb1fe28b90c04dfc344d871fcd79d6f14d7b047
Enrique Martinez <emartinez@evicertia.com>
2018-05-11 13:36:56 +0200

    registro primitivo

--- register.php
+++ register.php
@@ -3,30 +3,34 @@
 session_start();
 require_once('connect.php');
 if (isset($_POST) & !empty($_POST)) {	
-	
-	 $nusuario=$_POST["nUsuario"];	
-	 $userpassword=$_POST["userPassword"];
-	 $sql="SELECT * FROM `usuarios` WHERE nombreusuario='$nusuario' AND password='$userpassword'";
-	 //la tabla sin comillas o no funcionara la consulta
-	 $consulta=mysqli_query($connection,$sql);	 
+	print_r($_POST);
+	 $nombreusuario=$_POST["nombreusuario"];	
+	 $nombre=$_POST["nombre"];	
+	 $apellidos=$_POST["apellidos"];	
+	 $email=$_POST["email"];	
+	 $distrito=$_POST["distrito"];	
+	 $userpassword1=$_POST["password1"];
+	 $userpassword2=$_POST["password2"];
+	 if ($userpassword1==$userpassword2){
+	$sql="INSERT INTO `usuarios` (nombreusuario,password,nombre,apellidos,email) VALUES ('$nombreusuario','$userpassword1','$nombre','$apellidos','$email') ";
+	 $insercion=mysqli_query($connection,$sql);	 
+	 if($insercion){
+		 echo "El registro ha sido satisfactorio";
+	 } else {
+		 echo "El registro ha fallado miserablemente";
+	 }
 	 //si el usuario esta registrado el contador devolvera uno
-	 $contador=mysqli_num_rows($consulta);
-	 if ($contador==1) {
-		 $_SESSION['usuario']=$nusuario;
-	 }else{
-		 echo "Invalid Username/Password";
+	 //$contador=mysqli_num_rows($consulta);
+
+
 	 }
 }
-if(isset($_SESSION['usuario'])){
-	echo "Usuario: ".$_SESSION['usuario']." ya logeado";
-}
-
 ?>
 <html lang="es-ES">
 
 	<head>
 
-		<title>Maqueta Proyecto</title>
+		<title>Registro</title>
 
 		<meta charset="UTF-8">
 
@@ -42,21 +46,16 @@ if(isset($_SESSION['usuario'])){
         width: 50%;
         margin: 0 auto;
         }
-        div#Registro>*{
-        padding:5px;
+        div#Registro *{
+        margin:5px;
         }
         div#Registro>form{
-            display:flex;
-            flex-direction: column;
+		display:flex;
+		flex-direction: column;
         }
         </style>
 
 	</head>	
-    <style>
-    #registro{
-        display
-    }
-    </style>
 
 	<body>
       					<div id="pantallaRegistro">
@@ -72,7 +71,7 @@ if(isset($_SESSION['usuario'])){
 
       								<label>Nombre de usuario</label>
 
-      								<input type="text" name="nUsuario" placeholder="nombre de usuario">
+      								<input type="text" name="nombreusuario" placeholder="nombre de usuario">
 
       								<label>Nombre</label>
 
@@ -88,11 +87,25 @@ if(isset($_SESSION['usuario'])){
 
       								<label>Contraseña</label>
 
-      								<input type="password" name="userPassword">
+      								<input type="password" name="password1">
 
-      								<label>Repita la contraseña</label>
+      								<label>Confirma la contraseña</label>
 
-      								<input type="password" name="userPassword">
+      								<input type="password" name="password2">
+
+									<label>Selecciona tu distrito</label>
+
+									<select name="distrito">
+
+										<option value="arganzuela">arganzuela</option>
+
+										<option value="saab">Saab</option>
+
+										<option value="mercedes">Mercedes</option>
+
+										<option value="audi">Audi</option>
+
+									</select>
 
       								<button type="submit">Registrar<span>></span></button>
 
